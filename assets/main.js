591fc75f7c4187afbf9b4c7fb658ff3645262a7e
Elsti97 <elsti.shehati@hotmail.it>
2022-10-20 15:13:52 +0200

    Layout base "Calcolo biglietto"

---
+++ assets/main.js
@@ -0,0 +1,32 @@
+// // * Dati inziali
+
+// let km = prompt ("Quanti km vuoi percorrere?");
+
+// let età = prompt ("Quanti anni hai?");
+
+// const prezzo_km = 0.21;
+
+// // Prezzo base
+
+// let prezzo_totale = km * prezzo_km;
+
+// // Prezzo scontato 20% <18
+
+// let prezzo_minori = prezzo_totale * 0.8;
+
+// // Prezzo scontato 40% >65
+
+// let prezzo_senior = prezzo_totale * 0.6;
+
+// // ! IF
+
+// if (età <= 18){
+//   prezzo_totale =  prezzo_minori.toFixed(2);
+// } else if (età >= 65){
+//   prezzo_totale = prezzo_senior.toFixed(2);
+// } else {
+//   prezzo_totale = prezzo_totale
+// }
+
+
+// document.getElementById('prezzo_biglietto').innerHTML = "Costo biglietto è: "+ prezzo_totale + "€";
