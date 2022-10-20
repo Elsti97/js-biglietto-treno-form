8faa01975994d7c15c0ea49973ca1d95f5a02a57
Elsti97 <elsti.shehati@hotmail.it>
2022-10-20 17:24:36 +0200

    Button "genera" done

--- assets/main.js
+++ assets/main.js
@@ -3,7 +3,7 @@ let NomeUtente = document.getElementById('NomeUtente');
 
 let km = document.getElementById('Km');
 
-let età = document.getElementById('età');
+let eta = document.getElementById('eta');
 
 const prezzo_km = 0.21;
 
@@ -11,33 +11,41 @@ const prezzo_km = 0.21;
 // Dati generazione buttons
 let genera = document.getElementById('button-genera');
 
-let annulla = document.getElementById('button_annulla');
+let annulla = document.getElementById('button-annulla');
 
-// Prezzo base
 
-let prezzo_totale = km * prezzo_km;
+
+// IF BUTTON GENERA
+
+genera.addEventListener("click", function() {
+  console.log (NomeUtente)
+  console.log (km)
+  console.log (eta.value)
+
+  // Prezzo base
+
+  let prezzo_totale = km.value * prezzo_km;
 
 // Prezzo scontato 20% <18
 
-let prezzo_minori = prezzo_totale * 0.8;
+  let prezzo_minori = prezzo_totale * 0.8;
 
 // Prezzo scontato 40% >65
 
-let prezzo_senior = prezzo_totale * 0.6;
+  let prezzo_senior = prezzo_totale * 0.6;
 
-// ! IF
-
-genera.addEventListener("click", function() {
-
-    if (età.value === 'minorenne'){
+    if (eta.value === 'minorenne'){
     prezzo_totale =  prezzo_minori.toFixed(2);
-  } else if (età.value === 'senior'){
+  } else if (eta.value === 'senior'){
     prezzo_totale = prezzo_senior.toFixed(2);
   } else {
     prezzo_totale = prezzo_totale
   }
 
-  document.getElementById('prezzo_biglietto').innerHTML = "Costo biglietto è: "+ prezzo_totale + "€";
+  document.getElementById('NomeUtente').innerHTML = "Nominativo " + NomeUtente;
+  document.getElementById('prezzo_biglietto').innerHTML = "Costo biglietto è: " + prezzo_totale + "€";
   
 });
+
+
 
