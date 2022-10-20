a861e7b0154e01fd5d2c34d1bbc922267ee6886c
Elsti97 <elsti.shehati@hotmail.it>
2022-10-20 16:33:54 +0200

    Inserimento dati per button "genera"

--- assets/main.js
+++ assets/main.js
@@ -1,32 +1,44 @@
-// // * Dati inziali
+// * Dati inziali
+let NomeUtente = document.getElementById('NomeUtente');
 
-// let km = prompt ("Quanti km vuoi percorrere?");
+let km = document.getElementById('Km');
 
-// let età = prompt ("Quanti anni hai?");
+let età = document.getElementById('età');
 
-// const prezzo_km = 0.21;
-
-// // Prezzo base
-
-// let prezzo_totale = km * prezzo_km;
-
-// // Prezzo scontato 20% <18
-
-// let prezzo_minori = prezzo_totale * 0.8;
-
-// // Prezzo scontato 40% >65
-
-// let prezzo_senior = prezzo_totale * 0.6;
-
-// // ! IF
-
-// if (età <= 18){
-//   prezzo_totale =  prezzo_minori.toFixed(2);
-// } else if (età >= 65){
-//   prezzo_totale = prezzo_senior.toFixed(2);
-// } else {
-//   prezzo_totale = prezzo_totale
-// }
+const prezzo_km = 0.21;
 
 
-// document.getElementById('prezzo_biglietto').innerHTML = "Costo biglietto è: "+ prezzo_totale + "€";
+// Dati generazione buttons
+let button_genera = document.getElementById('genera');
+
+let button_annulla = document.getElementById('annulla');
+
+// Prezzo base
+
+let prezzo_totale = km * prezzo_km;
+
+// Prezzo scontato 20% <18
+
+let prezzo_minori = prezzo_totale * 0.8;
+
+// Prezzo scontato 40% >65
+
+let prezzo_senior = prezzo_totale * 0.6;
+
+// ! IF
+
+genera.addEventListener("click", function() {
+
+    if (età.value == 'minorenne'){
+    prezzo_totale =  prezzo_minori.toFixed(2);
+  } else if (età.value == 'senior'){
+    prezzo_totale = prezzo_senior.toFixed(2);
+  } else {
+    prezzo_totale = prezzo_totale
+  }
+
+  document.getElementById("demo").innerHTML = "Hello World";
+  document.getElementById('prezzo_biglietto').innerHTML = "Costo biglietto è: "+ prezzo_totale + "€";
+  
+});
+
